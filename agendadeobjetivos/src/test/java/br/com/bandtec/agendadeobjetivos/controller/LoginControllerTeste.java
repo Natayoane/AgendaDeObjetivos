c9411bc330c92c5d6f0e06ddcc221a4e7205306c
Natalia Tayoane <nataliatayoane@gmail.com>
2019-08-15 21:21:00 -0300

    Aula 1

---
+++ agendadeobjetivos/src/test/java/br/com/bandtec/agendadeobjetivos/controller/LoginControllerTeste.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.agendadeobjetivos.controller;
+
+public class LoginControllerTeste {
+	
+	public void loginComsucesso() {
+		LoginController controller = new LoginController();
+	}
+	
+	public void loginComFalha() {
+		
+	}
+
+}
