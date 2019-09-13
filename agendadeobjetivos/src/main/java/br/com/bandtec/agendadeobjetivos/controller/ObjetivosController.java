6d1148bf2ab305e02ee4e2c996e043e7eff480f4
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-09-12 23:02:26 -0300

    persistencia de dados

--- agendadeobjetivos/src/main/java/br/com/bandtec/agendadeobjetivos/controller/ObjetivosController.java
+++ agendadeobjetivos/src/main/java/br/com/bandtec/agendadeobjetivos/controller/ObjetivosController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,7 +22,7 @@ public class ObjetivosController {
 
     @PostMapping("/objetivos")
     public ResponseEntity<String> salvar(@RequestBody Objetivo objetivo) {
-        this.todosObjetivos.salvar(objetivo);
+        this.todosObjetivos.save(objetivo);
         return ResponseEntity.ok("Sucesso");
     }
 
